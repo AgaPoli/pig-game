3e0a5c01bb141ee8f7720014be8dcdd1d59c2d1a
AgaPoli <apolichnowska@tlen.pl>
2019-10-23 11:12:16 +0200

    Last updates in js

--- level2.js
+++ level2.js
@@ -6,7 +6,8 @@ var scores, roundScore, activePlayer, gamePlaying;
 var lastDice;
 init();
 
-dice = Math.floor(Math.random() * 6) + 1;  /*rzut kostka*/
+var dice1 = Math.floor(Math.random() * 6) + 1;  /*rzut kostka*/
+var dice2 = Math.floor(Math.random() * 6) + 1;  /*rzut kostka*/
 
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
@@ -16,18 +17,20 @@ document.querySelector('.dice').style.display = 'none'; // na poczatku zdjecie k
 
 document.querySelector('.btn-roll').addEventListener('click', function () {//funkcja to callback na event-bedzie wynik
    if (gamePlaying) {
-      var dice = Math.floor(Math.random() * 6) + 1; //losowanie wyniku
-      var diceDOM = document.querySelector('.dice'); /**zdjecie kostki */
-      diceDOM.style.display = 'block'; // odblokowanie zdjecia kostki po kliku
-      diceDOM.src = 'dice-' + dice + '.jpg'; // z jpg  variable(zmienna)
+      var dice1 = Math.floor(Math.random() * 6) + 1;  
+      var dice2 = Math.floor(Math.random() * 6) + 1;  /*rzut kostka*/ //losowanie wyniku
 
-      if (dice ===6 && lastDice ===6){
-        score[activePlayer]= 0;
-        document.querySelector('#score-' + activePlayer).textContent ='0';
-        nextPlayer() ;   
-      }else if  (dice !== 1) {
+      document.getElementById('dice-1').style.display='block'; /**zdjecie kostki */
+      document.getElementById('dice-2').style.display='block'; /**zdjecie kostki */ /**zdjecie kostki */
+      document.getElementById('dice-1').src = 'dice-' + dice1 + '.jpg'; // z jpg  variable(zmienna)
+      document.getElementById('dice-1').src = 'dice-' + dice2 + '.jpg';
+      // if (dice ===6 && lastDice ===6){
+      //   score[activePlayer]= 0;
+      //   document.querySelector('#score-' + activePlayer).textContent ='0';
+      //   nextPlayer() ;   
+      }if  (dice1 !== 1 && dice2 !==1) {
          //Add score
-         roundScore += dice; //update
+         roundScore += dice1 + dice2; //update
          //displaY in HTML
          document.querySelector('#current-' + activePlayer).textContent = roundScore; //display
 
@@ -35,9 +38,9 @@ document.querySelector('.btn-roll').addEventListener('click', function () {//fun
          nextPlayer();
       }
 
-      lastDice= dice;
+      // lastDice= dice;
    }
-});
+);
 
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
@@ -46,12 +49,22 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
       scores[activePlayer] += roundScore; //sumowanie pktow(nie ma 0 bo jest hold)
       //ui update spr gdzie sie zapisuja pkty sumy
       document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]; 
-
+      var input = document.querySelector('.final-score').value;
+      var winningScore;
+        
+      // Undefined, 0, null or "" are COERCED to false
+      // Anything else is COERCED to true
+      if(input) {
+         winningScore = input;
+     } else {
+         winningScore = 100;
+     }
 
       // spr czy gracz wygral gre czy ma 100 pkt
-      if (scores[activePlayer] >= 10) {
+      if (scores[activePlayer] >= 100) {
          document.querySelector('#name-' + activePlayer).textContent = 'Winner!';//tekst winner zamioanst pktopw u gory
-         document.querySelector('.dice').style.display = 'none';//znika kostka
+         document.getElementById('dice-1').style.display = 'none';
+         document.getElementById('dice-2').style.display = 'none';//znika kostka
          document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');//usowa kropke i pogrubuienie z css
          document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner'); //zmiana koloru z css
          gamePlaying = false;
@@ -72,7 +85,8 @@ function nextPlayer() {
    document.getElementById('current-1').textContent = '0';
    document.querySelector('.player-0-panel').classList.toggle('active'); //manipulacja klasami w HTML, zmiana podswietlenia katywnego gracza
    document.querySelector('.player-1-panel').classList.toggle('active'); //manipulacja klasami w HTML
-   document.querySelector('.dice').style.display = 'none'; // znika kostka
+   document.getElementById('dice-1').style.display = 'none';
+   document.getElementById('dice-2').style.display = 'none'; // znika kostka
    //document.querySelector('.player-0-panel ').classList.remove= 'active'; //manipulacja klasami w HTML
 
 }
@@ -86,7 +100,8 @@ function init() {
    activePlayer = 0;
    gamePlaying = true;
 
-   document.querySelector('.dice').style.display = 'none'; // na poczatku zdjecie kostki niewidoczne
+   document.getElementById('dice-1').style.display = 'block'; // na poczatku zdjecie kostki niewidoczne
+   document.getElementById('dice-2').style.display = 'block';
    document.getElementById('score-0').textContent = '0';
    document.getElementById('current-0').textContent = '0'; //activePlayer
    document.getElementById('score-1').textContent = '0';
